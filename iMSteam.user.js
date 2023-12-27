34e9249d23e6e4822b046c2e84e08ac56a274a62
DixonJRome <63641138+DixonJRome@users.noreply.github.com>
2023-12-27 23:29:41 +0300

    Update iMSteam.user.js
    Swapped rutracker and rutor. Increased the size of the buttons

--- iMSteam.user.js
+++ iMSteam.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Add search links for games on Steam pages
-// @author       iMAboud , daswer123 ( modified code )
+// @author       iMAboud , daswer123 (modified code), dixonjrome (modified the code for myself)
 // @match        https://store.steampowered.com/*
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -21,18 +21,24 @@
             const img = new Image();
             img.src = iconPath;
             img.alt = buttonText;
-            img.style.width = '64px';
-            img.style.height = '32px';
+            img.style.width = '128px';
+            img.style.height = '64px';
             img.style.objectFit = 'contain';
 
             linkButton.appendChild(img);
             linkButton.title = tooltipText;
-            linkButton.style.marginRight = '10px';
+            linkButton.style.marginRight = '30px';
             gameNameElement.parentNode.appendChild(linkButton);
         }
     }
 
     const formattedGameName = document.getElementById("appHubAppName").textContent.trim().toLowerCase().replace(/'/g, '').replace(/_/g, ' ');
+
+    const site1SearchLink = `https://rutracker.org/forum/tracker.php?nm=${formattedGameName}&o=10&s=2`;
+    createButton(site1SearchLink, "Rutracker", "Search on Rutracker", "https://i.imgur.com/UnHvprS.png");
+
+    const site3SearchLink = `http://rutor.info/search/1/0/000/2/${formattedGameName}`;
+    createButton(site3SearchLink, "Rutor", "Search on Rutor", "https://i.imgur.com/MBTI3G4.png");
 
     const steamDBSearchLink = `https://online-fix.me/index.php?do=search&subaction=search&story=${formattedGameName}`;
     createButton(steamDBSearchLink, "Online Fix", "Search on Online Fix", "https://i.imgur.com/WAXRAUw.png");
@@ -40,13 +46,7 @@
     const site4SearchLink = `https://small-games.info/?go=search&search_text=${formattedGameName}&sort=2`;
     createButton(site4SearchLink, "Small-games", "Search on Small-Games", "https://small-games.info/logo/logo.12.27.png");
 
-    const site1SearchLink = `https://rutracker.org/forum/tracker.php?nm=${formattedGameName}&o=10&s=2`;
-    createButton(site1SearchLink, "Rutracker", "Search on Rutracker", "https://i.imgur.com/UnHvprS.png");
-
     const site2SearchLink = `https://nnmclub.to/forum/tracker.php?nm=${formattedGameName}&o=10&s=2`;
     createButton(site2SearchLink, "NnmClub", "Search on NnmClub", "https://i.imgur.com/s8eRrZr.png");
-
-    const site3SearchLink = `http://rutor.info/search/1/0/000/2/${formattedGameName}`;
-    createButton(site3SearchLink, "Rutor", "Search on Rutor", "https://i.imgur.com/MBTI3G4.png");
 
 })();
